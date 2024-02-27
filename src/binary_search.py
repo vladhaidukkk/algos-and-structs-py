141ef6f430a5735d4618da958190d8d0c5933d71
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2024-02-27 09:37:42 +0200

    implement binary search

--- src/binary_search.py
+++ src/binary_search.py
@@ -1,2 +1,16 @@
 def binary_search(xs, target):
-    pass
+    low = 0
+    high = len(xs) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = xs[mid]
+
+        if guess == target:
+            return mid
+        elif guess < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return None
